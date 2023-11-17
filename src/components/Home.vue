db4a0071cce31bc2eb3b7f132ec59831fba2e1a9
Osmeily <osmeilypernia@gmail.com>
2023-11-17 12:02:22 -0500

    project structure

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume
+            :label="'Ahorro total'"
+            :date-label="dateLabel"
+            :total-amount="100000"
+            :amount="amount"
+            >
+                <template #graphic>Graphic</template>
+                <template #action>Action</template>
+            </Resume>
+        </template>
+        <template #movements>
+            <Movements/>
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from '@/components/resume/Index.vue'
+import Movements from "./Movements.vue";
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements,
+    },
+    data() {
+        return {
+            amount: null,
+            dateLabel: null,
+        }
+    }
+}
+</script>
